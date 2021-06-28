a40f1e6529d97ac9c83ad28c64f136d3857eb533
AnastasiaTsvetkova <86355641+AnastasiaTsvetkova@users.noreply.github.com>
2021-06-28 14:06:00 +0300

    Add files via upload

---
+++ LR.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream> 
+#include <string> 
+using namespace std; 
+void str() 
+{ 
+string a, a1, a2; 
+cout << "Введите строку - "; 
+getline(cin, a); 
+cout << "Введите подстроку - "; 
+getline(cin, a1); 
+cout << "Введите свою подстроку - "; 
+getline(cin, a2); 
+a.replace(a.find(a1), a1.size(), a2); 
+cout << a << " - строка" << endl; 
+} 
+int main() 
+{ 
+setlocale(LC_ALL, "RU"); 
+str(); 
+system("pause"); 
+return 0; 
+} 
+
